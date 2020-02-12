2707b28aa659b98c3d565491e391a4889787d8c8
Katsuya50 <katsuya.fujishima@rakus-partners.co.jp>
2020-02-12 09:23:20 +0900

    transactionalアノテーションの追加

--- src/main/java/com/example/service/TeamService.java
+++ src/main/java/com/example/service/TeamService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.domain.Team;
 import com.example.repository.TeamRepository;
@@ -15,6 +16,7 @@ import com.example.repository.TeamRepository;
  *
  */
 @Service
+@Transactional
 public class TeamService {
 	
 	@Autowired
