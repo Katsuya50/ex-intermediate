a7a598e48ed093ba432928194f51138142a84160
Katsuya50 <katsuya.fujishima@rakus-partners.co.jp>
2020-02-12 12:04:53 +0900

    JavaDocの追加

--- src/main/java/com/example/controller/HotelController.java
+++ src/main/java/com/example/controller/HotelController.java
@@ -10,6 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.example.domain.Hotel;
 import com.example.service.HotelService;
 
+/**
+ * ホテル検索アプリのコントローラークラス.
+ * 
+ * @author katsuya.fujishima
+ *
+ */
 @Controller
 @RequestMapping("/")
 public class HotelController {
@@ -17,11 +23,23 @@ public class HotelController {
 	@Autowired
 	private HotelService service;
 	
+	/**
+	 * URL指定時に検索画面にフォワードするメソッド.
+	 * 
+	 * @return 検索画面
+	 */
 	@RequestMapping("/")
 	public String index() {
 		return "search";
 	}
 	
+	/**
+	 * 検索結果を表示するメソッド.
+	 * 
+	 * @param price 価格
+	 * @param model リクエストスコープ
+	 * @return 検索結果を表示した検索画面
+	 */
 	@RequestMapping("/search")
 	public String search(Integer price, Model model) {
 		List<Hotel> hotelList = service.searchByLessThanPrice(price);
