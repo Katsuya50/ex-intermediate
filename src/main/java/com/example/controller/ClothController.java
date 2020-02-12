b92aed73dbced69f93d53102a5b1e791b1211c1d
Katsuya50 <katsuya.fujishima@rakus-partners.co.jp>
2020-02-12 15:40:11 +0900

    軽微な修正

--- src/main/java/com/example/controller/ClothController.java
+++ src/main/java/com/example/controller/ClothController.java
@@ -8,7 +8,6 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.domain.Cloth;
